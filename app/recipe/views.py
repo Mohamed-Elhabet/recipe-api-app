da65a2a36d803cfd166c9ad54ef3689d6d6726d5
Mohamed-Elhabet <melhabet180@gmail.com>
2024-12-14 14:31:24 +0200

    Implement for filter recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,18 +6,52 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema_view, extend_schema, \
+    OpenApiParameter, OpenApiTypes
+    
 
 
 # Create your views here.
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags', OpenApiTypes.STR,
+                description='Comma separated list of IDs to filter'
+            ),
+            OpenApiParameter(
+                'ingredients', OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter'
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers.
+            qs = '1,2,3,4' -> [1, 2, 3, 4]
+        """
+        return [int(str_id) for str_id in qs.split(',')]
+    
+    
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+            
+        return queryset.filter(user=self.request.user).order_by('-id').distinct()
     
     
     def get_serializer_class(self):
